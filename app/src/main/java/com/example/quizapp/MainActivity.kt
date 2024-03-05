02a078e66d4be068274df0c1555c2f4a487a5b54
Moises Peña Anaya <moises.pena0176@alumnos.udg.mx>
2024-03-04 20:21:17 -0600

    preguntas con toast

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.quizapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val questions = listOf(
+        "¿Android Studio es un IDE de desarrollo?",
+        "¿Java es un lenguaje de programación?",
+        "¿XML se utiliza para diseñar interfaces en Android?",
+        "¿Kotlin es oficialmente compatible con Android?",
+        "¿Android es desarrollado por Apple?"
+    )
+
+    private val answers = listOf(
+        true, true, true, true, false
+    )
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,14 +29,36 @@ class MainActivity : AppCompatActivity() {
         val trueButton = findViewById<Button>(R.id.trueButton)
         val falseButton = findViewById<Button>(R.id.falseButton)
 
+        showQuestion()
+
         trueButton.setOnClickListener {
-            // Acción cuando se presiona Verdadero
-            showToast("Bien Hecho...!")
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener {
-            // Acción cuando se presiona Falso
-            showToast("No mi compa...")
+            checkAnswer(false)
+        }
+    }
+
+    private fun showQuestion() {
+        findViewById<TextView>(R.id.questionTextView).text = questions[currentIndex]
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = answers[currentIndex]
+
+        if (userAnswer == correctAnswer) {
+            showToast("¡Respuesta Correcta!")
+        } else {
+            showToast("Respuesta Incorrecta :(")
+        }
+
+        currentIndex++
+
+        if (currentIndex < questions.size) {
+            showQuestion()
+        } else {
+            currentIndex = 0
         }
     }
 
